3b1ba1db1064bf0dc001296d3d2d8bd89f0832e8
bosshp2000 <lephuc260205@gmail.com>
2025-06-11 22:28:07 +0700

    Them @RequestMapping() vao trong Controller Dashboard de quan ly api

--- backend/src/main/java/com/dominator/bookify/controller/admin/AdminDashboardControllerImplement.java
+++ backend/src/main/java/com/dominator/bookify/controller/admin/AdminDashboardControllerImplement.java
@@ -4,41 +4,44 @@ import com.dominator.bookify.dto.*;
 import com.dominator.bookify.service.admin.AdminDashboardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/admin/dashboard")
+
 public class AdminDashboardControllerImplement implements AdminDashboardController {
 
     private final AdminDashboardService dashboardService;
 
-    @GetMapping("/api/admin/dashboard/top-by-category")
+    @GetMapping("/top-by-category")
     @Override
     public List<TopCategoryQuantityDTO> getTopQuantityPerCategory() {
         return dashboardService.getTop10BooksPerCategory();
     }
 
-    @GetMapping("/api/admin/dashboard/top-books")
+    @GetMapping("/top-books")
     @Override
     public List<BestSellerDTO> getTopBooks() {
         return dashboardService.getTop10BestSellingBooks();
     }
 
-    @GetMapping("api/admin/dashboard/loyal-customers")
+    @GetMapping("/loyal-customers")
     @Override
     public List<LoyalCustomerDTO> getTopLoyalCustomers() {
         return dashboardService.getTop10LoyalCustomers();
     }
 
-    @GetMapping("api/admin/dashboard/top-user-by-avg-order-value")
+    @GetMapping("/top-user-by-avg-order-value")
     @Override
     public TopAvgOrderValueUserDTO getTopAvgOrderValueUser() {
         return dashboardService.findUserWithHighestAvgOrderValue();
     }
 
-    @GetMapping("api/admin/dashboard/book-in-low-stock")
+    @GetMapping("/book-in-low-stock")
     @Override
     public List<BookInLowStockDTO> getTopBooksInLowStock() {
         return dashboardService.getBookWithLowStock();
